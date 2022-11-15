71d2d1bdfb1d83bfa9d32e43c1b8fdbd66fba735
Tao <tao.spa2003@gmail.com>
2022-11-15 17:35:45 +0100

    adding files and constents for the alt forms

---
+++ flechi.c
@@ -0,0 +1,5 @@
+//
+// Created by Tao SAINT PAUL AMOURDAM on 15/11/2022.
+//
+
+#include "flechi.h"
---
+++ flechi.h
@@ -0,0 +1,22 @@
+//
+// Created by Tao SAINT PAUL AMOURDAM on 15/11/2022.
+//
+
+#ifndef CADAVRE_EXQUIS_FLECHI_H
+#define CADAVRE_EXQUIS_FLECHI_H
+#define Mas 1
+#define Fem 2
+#define InvGen 3
+
+#define SG 10
+#define PL 20
+
+#define P1 100
+#define P2 200
+#define P3 300
+
+#define IPre 1000
+#define IImp 2000
+#define SPre 3000
+
+#endif //CADAVRE_EXQUIS_FLECHI_H
