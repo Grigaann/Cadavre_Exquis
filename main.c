279656ecaef55920bfae87cbc859de8b27863ef7
moyaxzan <tao.spa2003@gmail.com>
2022-11-20 12:28:41 +0100

    better menu

--- main.c
+++ main.c
@@ -9,31 +9,30 @@ int main() {
     int wrong_ans=1;
     char wordeu[100];
     int ans_menu1, ans_menu2;
+    int bool = 1;
 
     tree_list trees = initialize_trees();
-    printf("Welcome in this simulation.\n");
-    while(1) {
-        printf("\n          This is the menu :\n\n"
-               "Enter 1 if you wish to see the tree\n"
-               "Enter 2 if you wish to search for a word\n"
+    printf("Welcome in the Automatic sentences generator.\n\n");
+    while(bool) {
+        printf("Enter :\n1 if you wish to see the tree\n"
+               "2 if you wish to search for a word\n"
                //"Enter 3 if you wish to create a homemade sentence\n"
-               "4 for random word\n"
-               "Enter 0 if you want to exit this program\n"
-               " Your choice :");
+               "4 to get a random word\n"
+               "\n0 if you want to exit this program\n\n");
         scanf("%d", &ans_menu1);
         switch (ans_menu1) {
             case 0:
                 printf("See you later.");
-                exit(3);
+                bool = 0;
+                break;
             case 1:
                 while (wrong_ans){
                     printf("\nWhich tree do you wish to see ?\n\n"
-                           "Enter 1 if you wish to see the names\n"
-                           "Enter 2 if you wish to see the adjectives\n"
-                           "Enter 3 if you wish to see the verbs\n"
-                           "Enter 4 if you wish to see the adverbs\n"
-                           "Enter 0 if you want to exit this menu\n"
-                           " Your choice :");
+                           "Enter : \n1 if you wish to see the names\n"
+                           "2 if you wish to see the adjectives\n"
+                           "3 if you wish to see the verbs\n"
+                           "4 if you wish to see the adverbs\n"
+                           "0 if you want to go back\n");
                     scanf("%d", &ans_menu2);
                     switch (ans_menu2) {
                         case 0:
